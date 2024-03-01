d2dde85495475bbe3f57e0b927b33c062b0dcf21
OLVOS <anhkrem@gmail.com>
2024-03-01 22:42:24 +0300

    amend to commit ad32e1e with brain_game.py

--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # добавляю cli в sys.path
 import sys
-sys.path.append('/home/python-project-49/brain_games/scripts')
+sys.path.append('/home/python-project-49/brain_games')
 from cli import welcome_user
 
 
