d6730f377af225809a4da2d1e4a9b3de39792580
OLVOS <anhkrem@gmail.com>
2024-03-03 01:25:51 +0300

    Fix hexlet test with stupid pt.3

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,23 +4,21 @@ from brain_games.cli import welcome_user, user_name
 def game(rules, que_ans):
     print(rules)
 
-    correct_ans = 0
     for i, num in enumerate(que_ans):
         que = que_ans[i]["question"]
         ans = que_ans[i]["answer"]
         wrong_txt = "is wrong answer ;(. Correct answer was"
 
-        if correct_ans != 3:
-            print(f'Question: {que}')
-            usr_ans = input('Your answer: ')
+        print(f'Question: {que}')
+        usr_ans = input('Your answer: ')
 
-            if usr_ans == ans:
-                print('Correct!')
-                correct_ans += 1
+        if usr_ans == ans:
+            print('Correct!')
+            correct_ans += 1
 
-            else:
-                print(f'"{usr_ans}" {wrong_txt} "{ans}".\n'
-                      f'Let\'s try again, {user_name[0]}!')
+        else:
+            print(f'"{usr_ans}" {wrong_txt} "{ans}".\n'
+                  f'Let\'s try again, {user_name[0]}!')
 
     print(f'Congratulations, {user_name[0]}!')
 
