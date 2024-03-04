5e6a1dd78abdd4a98af39489ccae27032d070e2b
OLVOS <anhkrem@gmail.com>
2024-03-04 20:38:12 +0300

    Fix Codeclimate issues with refactoring brain-prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -5,31 +5,41 @@ from random import choice
 prime_rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def prime_question_answer():
+def dividers_list(number):
+    dividers = []
+    for index, number in enumerate(number):
+        dividers.append([i for i in range(2, number)])
+    return dividers
+
+
+def prime_question_answer(dividers_lst):
     result = []
+
     rnd_nums = [choice(range(2, 100)) for i in range(3)]
+    dividers = dividers_lst(rnd_nums)
 
     for index, number in enumerate(rnd_nums):
-        dividers = [i for i in range(2, number)]
+        for i, num in enumerate(dividers[index]):
+            last_number = dividers[index][-1]
+            current_div = dividers[index][i]
 
-        for ind, num in enumerate(dividers):
-
-            if number % num == 0:
+            if number % current_div == 0:
                 result.append({'question': f'{number}', 'answer': 'no'})
                 break
 
-            elif num != dividers[-1]:
-                continue
-
-            else:
+            elif current_div == last_number:
                 result.append({'question': f'{number}', 'answer': 'yes'})
                 break
 
     return result
 
 
+def prime_final():
+    return prime_question_answer(dividers_list)
+
+
 def main():
-    game(prime_rules, prime_question_answer())
+    game(prime_rules, prime_final())
 
 
 if __name__ == '__main__':
