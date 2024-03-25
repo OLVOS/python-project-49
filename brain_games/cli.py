9deeab718fd6f1039fcf45f430c08877c1beed69
OLVOS <anhkrem@gmail.com>
2024-03-25 12:15:21 +0300

    Amend to commit bd02418, small fix 3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,20 +1,7 @@
 import prompt
 
 
-user_name = []
-
-
 def welcome_user():
-    if len(user_name) < 1:
-        print('Welcome to the Brain Games!')
-        name_ask = prompt.string('May I have your name? ')
-        user_name.append(f'{name_ask}')
-        print(f'Hello, {user_name[0]}!')
-
-
-def main():
-    welcome_user()
-
-
-if __name__ == '__main__':
-    main()
+    print('Welcome to the Brain Games!')
+    name_ask = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name[0]}!')
