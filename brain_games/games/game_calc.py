f5529bba08a6d35733b3d30aaab2f236d52c9ae8
OLVOS <anhkrem@gmail.com>
2024-05-22 18:27:40 +0300

    rename func in /games

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -3,7 +3,7 @@ from brain_games.consts import CALC_RULES, MATH_SIGNS
 from random import choice, randint
 
 
-def get_math_question_and_result():
+def get_math_expression_and_result():
     num1, num2 = randint(1, 100), randint(1, 100)
     math_sign = choice(MATH_SIGNS)
     math_expression = f'{num1} {math_sign} {num2}'
@@ -13,4 +13,4 @@ def get_math_question_and_result():
 
 
 def run_game_calc():
-    run_game(CALC_RULES, get_math_question_and_result)
+    run_game(CALC_RULES, get_math_expression_and_result)
--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -3,11 +3,11 @@ from brain_games.consts import EVEN_RULES
 from random import randint
 
 
-def get_string_question_and_answer():
+def get_num_and_even_status():
     question = randint(1, 100)
     answer = 'yes' if question % 2 == 0 else 'no'
     return [question, answer]
 
 
 def run_game_even():
-    run_game(EVEN_RULES, get_string_question_and_answer)
+    run_game(EVEN_RULES, get_num_and_even_status)
